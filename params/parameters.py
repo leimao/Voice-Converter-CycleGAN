b47db8d470703bb9968006bc30f85ccc27a1b74d
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-02 14:05:42 +0000

    Add dataset directories

--- params/parameters.py
+++ params/parameters.py
@@ -5,6 +5,12 @@ num_epochs = 1000
 sampling_rate = 16000
 
 #############################
+# Datsets Parameters
+#############################
+train_A_dir = './content/drive/MyDrive/Dataset/Emotional Speech Audio/Audios/neutral'
+train_B_dir = './content/drive/MyDrive/Dataset/Emotional Speech Audio/Audios/happy'
+
+#############################
 # Model Parameters
 #############################
 model_dir ='./model'
