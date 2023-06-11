c63456d49e1b087e510e4bf3e954800c6e873203
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-11 23:59:54 +0000

    Revert to old model architecture

--- utils.py
+++ utils.py
@@ -5,15 +5,13 @@ import numpy as np
 import tensorflow as tf
 
 def l1_loss(y, y_hat):
-    y = tf.cast(y, dtype=tf.float32)  # Cast y to float32
-    y_hat = tf.cast(y_hat, dtype=tf.float32)  # Cast y_hat to float32
     loss = tf.reduce_mean(tf.abs(y - y_hat))
     return loss
 
 
 def l2_loss(y, y_hat):
-    return tf.losses.mean_squared_error((y, y_hat))
+    return tf.losses.mean_squared_error(y, y_hat)
 
 def cross_entropy_loss(logits, labels):
-    return tf.losses.sigmoid_cross_entropy((labels, logits))
+    return tf.losses.sigmoid_cross_entropy(labels, logits)
 
