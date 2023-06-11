a8aea1d7d237d988c47a080ece4317c70f6ad584
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-11 19:31:45 +0000

    Modify l1 loss function

--- utils.py
+++ utils.py
@@ -7,13 +7,13 @@ import tensorflow as tf
 def l1_loss(y, y_hat):
     y = tf.cast(y, tf.float32)
     y_hat = tf.cast(y_hat, tf.float32)
-    loss = tf.reduce_mean(tf.math.abs(y - y_hat))
+    loss = tf.reduce_mean(tf.math.abs(y - y_hat),axis=[1, 2])
     return loss
 
 
 def l2_loss(y, y_hat):
-    return tf.losses.mean_squared_error(y, y_hat)
+    return tf.losses.mean_squared_error((y, y_hat),axis=[1, 2])
 
 def cross_entropy_loss(logits, labels):
-    return tf.losses.sigmoid_cross_entropy(labels, logits)
+    return tf.losses.sigmoid_cross_entropy((labels, logits),axis=[0, 2])
 
