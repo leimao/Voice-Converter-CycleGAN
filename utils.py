05a62b73f1934b97f3a0d8a84eb13f5337d96dee
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-11 19:45:44 +0000

    Modify l1 loss function

--- utils.py
+++ utils.py
@@ -12,8 +12,8 @@ def l1_loss(y, y_hat):
 
 
 def l2_loss(y, y_hat):
-    return tf.losses.mean_squared_error((y, y_hat),axis=[1, 2])
+    return tf.losses.mean_squared_error((y, y_hat))
 
 def cross_entropy_loss(logits, labels):
-    return tf.losses.sigmoid_cross_entropy((labels, logits),axis=[0, 2])
+    return tf.losses.sigmoid_cross_entropy((labels, logits))
 
