3aa22bcd87079680b004ee0c6df7cd3ccdb2c6aa
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-12 00:36:01 +0000

    Change loss functions

--- utils.py
+++ utils.py
@@ -1,17 +1,16 @@
-
+import tensorflow as tf
 import os
 import random
 import numpy as np
-import tensorflow as tf
 
 def l1_loss(y, y_hat):
-    loss = tf.reduce_mean(tf.abs(y - y_hat))
-    return loss
-
+    print( tf.math.reduce_mean(tf.math.abs(y - y_hat)))
+    return tf.math.reduce_mean(tf.math.abs(y - y_hat))
 
 def l2_loss(y, y_hat):
-    return tf.losses.mean_squared_error(y, y_hat)
+
+    return tf.math.reduce_mean(tf.math.square(y - y_hat))
 
 def cross_entropy_loss(logits, labels):
-    return tf.losses.sigmoid_cross_entropy(labels, logits)
+    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = logits, labels = labels))
 
