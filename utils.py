46db9d3ebf194349614b7e90b6a62a5cd0ebc266
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-13 10:12:40 +0000

    Fix loss functions to handle tensors of any shape

--- utils.py
+++ utils.py
@@ -11,8 +11,13 @@ def pad_tensor(tensor, target_dim):
     return padded_tensor
 
 def l1_loss(y, y_hat):
-    last_dim_y = y.get_shape().as_list()[-1]
-    last_dim_y_hat = y_hat.get_shape().as_list()[-1]
+    shape_y = tf.shape(y)
+    shape_y_hat = tf.shape(y_hat)
+
+    last_dim_y = y.get_shape().as_list()[-1] if y.get_shape().ndims is not None else shape_y[-1]
+    last_dim_y_hat = y_hat.get_shape().as_list()[-1] if y_hat.get_shape().ndims is not None else shape_y_hat[-1]
+
+
 
     if last_dim_y < last_dim_y_hat:
         padded_y = pad_tensor(y, last_dim_y_hat)
@@ -28,8 +33,12 @@ def l1_loss(y, y_hat):
     return loss
 
 def l2_loss(y, y_hat):
-    last_dim_y = y.get_shape().as_list()[-1]
-    last_dim_y_hat = y_hat.get_shape().as_list()[-1]
+    shape_y = tf.shape(y)
+    shape_y_hat = tf.shape(y_hat)
+
+    last_dim_y = y.get_shape().as_list()[-1] if y.get_shape().ndims is not None else shape_y[-1]
+    last_dim_y_hat = y_hat.get_shape().as_list()[-1] if y_hat.get_shape().ndims is not None else shape_y_hat[-1]
+
 
     if last_dim_y < last_dim_y_hat:
         padded_y = pad_tensor(y, last_dim_y_hat)
