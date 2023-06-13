5cb765740440615508962fe7212129d715638d49
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-13 10:02:23 +0000

    Fix loss functions to handle tensors of any shape

--- utils.py
+++ utils.py
@@ -1,16 +1,45 @@
 import tensorflow as tf
-import os
-import random
 import numpy as np
 
+
+def pad_tensor(tensor, target_dim):
+    shape = tf.shape(tensor)
+    last_dim = tensor.get_shape().as_list()[-1]
+    padding = target_dim - last_dim
+    padding_shape = tf.concat([shape[:-1], [padding]], axis=0)
+    padded_tensor = tf.concat([tensor, tf.zeros(padding_shape)], axis=-1)
+    return padded_tensor
+
 def l1_loss(y, y_hat):
-    print( tf.math.reduce_mean(tf.math.abs(y - y_hat)))
-    return tf.math.reduce_mean(tf.math.abs(y - y_hat))
+    last_dim_y = y.get_shape().as_list()[-1]
+    last_dim_y_hat = y_hat.get_shape().as_list()[-1]
+
+    if last_dim_y < last_dim_y_hat:
+        padded_y = pad_tensor(y, last_dim_y_hat)
+        padded_y_hat = y_hat
+    elif last_dim_y_hat < last_dim_y:
+        padded_y = y
+        padded_y_hat = pad_tensor(y_hat, last_dim_y)
+    else:
+        padded_y = y
+        padded_y_hat = y_hat
+
+    loss = tf.reduce_mean(tf.abs(padded_y - padded_y_hat))
+    return loss
 
 def l2_loss(y, y_hat):
+    last_dim_y = y.get_shape().as_list()[-1]
+    last_dim_y_hat = y_hat.get_shape().as_list()[-1]
 
-    return tf.math.reduce_mean(tf.math.square(y - y_hat))
+    if last_dim_y < last_dim_y_hat:
+        padded_y = pad_tensor(y, last_dim_y_hat)
+        padded_y_hat = y_hat
+    elif last_dim_y_hat < last_dim_y:
+        padded_y = y
+        padded_y_hat = pad_tensor(y_hat, last_dim_y)
+    else:
+        padded_y = y
+        padded_y_hat = y_hat
 
-def cross_entropy_loss(logits, labels):
-    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = logits, labels = labels))
-
+    loss = tf.reduce_mean(tf.square(padded_y - padded_y_hat))
+    return loss
