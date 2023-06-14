2c8665ca9dfdf7e98d3333ac171799dab04d5666
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-14 01:46:03 +0000

    Edit loss functions

--- utils.py
+++ utils.py
@@ -8,23 +8,23 @@ def pad_tensor(tensor, target_dim):
     return padded_tensor
 
 def l1_loss(y, y_hat):
-    last_dim_y = y.get_shape().as_list()[-1]
-    last_dim_y_hat = y_hat.get_shape().as_list()[-1]
+    last_dim_y = tf.shape(y)[-1]
+    last_dim_y_hat = tf.shape(y_hat)[-1]
 
     # Pad tensors to match the larger dimension
-    padded_y = pad_tensor(y, max(last_dim_y, last_dim_y_hat))
-    padded_y_hat = pad_tensor(y_hat, max(last_dim_y, last_dim_y_hat))
+    padded_y = pad_tensor(y, tf.maximum(last_dim_y, last_dim_y_hat))
+    padded_y_hat = pad_tensor(y_hat, tf.maximum(last_dim_y, last_dim_y_hat))
 
     loss = tf.reduce_mean(tf.abs(tf.subtract(padded_y, padded_y_hat)))
     return loss
 
 def l2_loss(y, y_hat):
-    last_dim_y = y.get_shape().as_list()[-1]
-    last_dim_y_hat = y_hat.get_shape().as_list()[-1]
+    last_dim_y = tf.shape(y)[-1]
+    last_dim_y_hat = tf.shape(y_hat)[-1]
 
     # Pad tensors to match the larger dimension
-    padded_y = pad_tensor(y, max(last_dim_y, last_dim_y_hat))
-    padded_y_hat = pad_tensor(y_hat, max(last_dim_y, last_dim_y_hat))
+    padded_y = pad_tensor(y, tf.maximum(last_dim_y, last_dim_y_hat))
+    padded_y_hat = pad_tensor(y_hat, tf.maximum(last_dim_y, last_dim_y_hat))
 
     loss = tf.reduce_mean(tf.square(tf.subtract(padded_y, padded_y_hat)))
     return loss
