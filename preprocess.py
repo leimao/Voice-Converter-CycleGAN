2cac715df68baa4db4b3dec9860fc93f421bce5a
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-18 23:48:01 +0000

    Refactor pitch function

--- preprocess.py
+++ preprocess.py
@@ -179,15 +179,17 @@ def logf0_statistics(f0s):
     return log_f0s_mean, log_f0s_std
 
 def pitch_conversion(f0, mean_log_src, std_log_src, mean_log_target, std_log_target):
-    EPSILON = 1e-10  # Small constant value
-    
-    # Broadcasting the arrays to have compatible shapes
-    mean_log_src = mean_log_src.squeeze()
-    std_log_src = std_log_src.squeeze()
-    mean_log_target = mean_log_target.squeeze()
-    std_log_target = std_log_target.squeeze()
-    
+    # Reshape the arrays to have compatible shapes
+    f0 = f0.reshape((-1,))
+    mean_log_src = mean_log_src.reshape((-1,))
+    std_log_src = std_log_src.reshape((-1,))
+    mean_log_target = mean_log_target.reshape((-1,))
+    std_log_target = std_log_target.reshape((-1,))
+
+    # Perform element-wise calculations
+    EPSILON = 1e-10
     f0_converted = np.exp((np.log(f0 + EPSILON) - mean_log_src) / std_log_src * std_log_target + mean_log_target)
+
     return f0_converted
 
 def wavs_to_specs(wavs, n_fft = 1024, hop_length = None):
