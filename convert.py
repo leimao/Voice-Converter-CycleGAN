Fix normalization file names mismatch

convert.py
@@ -37,18 +37,6 @@ def conversion(file, conversion_direction='A2B'):
     wav, _ = librosa.load(file, sr = sampling_rate, mono = True)
     wav = wav_padding(wav = wav, sr = sampling_rate, frame_period = frame_period, multiple = 4)
     f0, timeaxis, sp, ap = world_decompose(wav = wav, fs = sampling_rate, frame_period = frame_period)
-    print('Shape of mcep_mean_A: {}'.format(mcep_mean_A.shape))
-    print('Shape of mcep_std_A: {}'.format(mcep_std_A.shape))
-    print('Shape of mcep_mean_B: {}'.format(mcep_mean_B.shape))
-    print('Shape of mcep_std_B: {}'.format(mcep_std_B.shape))
-
-    print()
-    print('Shape of logf0s_mean_A: {}'.format(logf0s_mean_A.shape))
-    print('Shape of logf0s_std_A: {}'.format(logf0s_std_A.shape))
-    print('Shape of logf0s_mean_B: {}'.format(logf0s_mean_B.shape))
-    print('Shape of logf0s_std_B: {}'.format(logf0s_std_B.shape))
-
-    print()
 
     coded_sp = world_encode_spectral_envelop(sp = sp, fs = sampling_rate, dim = num_features)
 
@@ -74,7 +62,7 @@ def conversion(file, conversion_direction='A2B'):
     coded_sp_converted = np.ascontiguousarray(coded_sp_converted)
     decoded_sp_converted = world_decode_spectral_envelop(coded_sp = coded_sp_converted, fs = sampling_rate)
 
-    wav_transformed = world_speech_synthesis(f0 = f0, decoded_sp = decoded_sp_converted, ap = ap, fs = sampling_rate, frame_period = frame_period)
+    wav_transformed = world_speech_synthesis(f0 = f0_converted, decoded_sp = decoded_sp_converted, ap = ap, fs = sampling_rate, frame_period = frame_period)
 
     visualize_audio(wav,sampling_rate,'Monotone audio')
     visualize_audio(wav_transformed,sampling_rate,'Synthesised audio')